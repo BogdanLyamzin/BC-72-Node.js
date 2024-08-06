3570ad1d88749afa813df7e39502b0bd9ba4e4ae
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2024-08-06 19:08:06 +0300

    finish lesson 3

---
+++ lesson-3/express-example-4/routes/moviesRouter.js
@@ -0,0 +1,28 @@
+import {Router} from "express";
+
+import movies from "../data/movies.js";
+
+const moviesRouter = Router();
+
+moviesRouter.get("/", (req, res)=> {
+    res.json(movies);
+})
+
+moviesRouter.get("/:id", (req, res)=> {
+    res.json(movies[0]);
+})
+
+moviesRouter.post("/", (req, res)=> {
+    res.json(movies[0]);
+})
+
+moviesRouter.put("/:id", (req, res)=> {
+    res.json(movies[0]);
+})
+
+moviesRouter.delete("/:id", (req, res)=> {
+    res.json(movies[0]);
+})
+
+
+export default moviesRouter;
